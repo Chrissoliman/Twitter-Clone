979632d9dab3ac8d9917073c30f86639f29e1f88
Chris Soliman <chris201020112@gmail.com>
2024-07-14 15:37:19 +0300

    1- Auth Controllers Added

---
+++ backend/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const { model, models, Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    password: { type: String, required: true, minLength: 6 },
+    email: { type: String, required: true, unique: true },
+    followers: [{type: Schema.Types.ObjectId, ref: 'User', default: []}],
+    following: [{type: Schema.Types.ObjectId, ref: 'User', default: []}],
+    profileImg: {type: String, default: ''},
+    coverImg: {type: String, default: ''},
+    bio: {type: String, default: ''},
+    link: {type: String, default: ''},
+  },
+  { timestamps: true }
+);
+
+const User = models?.User || model('User', userSchema)
+
+export default User
