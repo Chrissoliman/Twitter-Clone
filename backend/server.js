0c483d21d5a99190b966c248501c4c4b8e565065
Chris Soliman <chris201020112@gmail.com>
2024-07-18 19:04:40 +0300

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json());
+app.use(express.json({limit: '5mb'})); // limit by default is 100kb so it will not take image, must make limit not too high because of DOS attacks
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
